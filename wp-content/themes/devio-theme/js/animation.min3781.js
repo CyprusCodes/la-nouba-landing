1a83c2310860dafdc0c1b14143f08f18c261a0d5
teddy-webdev <89258830+teddy-webdev@users.noreply.github.com>
2023-07-05 13:17:43 +0300

    wip
    wip

--- wp-content/themes/devio-theme/js/animation.min3781.js
+++ wp-content/themes/devio-theme/js/animation.min3781.js
@@ -1,1 +1,83 @@
-jQuery(function(){0<$(".devio__animate").length&&gsap.utils.toArray(".devio__animate").forEach(e=>{var t=e.getAttribute("data-animate");t&&("fade-up"===t?gsap.to(e,{scrollTrigger:{trigger:e.closest("section"),start:"20% 60%",end:"20% 60%",toggleActions:"play none reverse none"},duration:1,opacity:1,y:0,transformOrigin:"0% 50% -50",ease:Back.easeOut,stagger:.02}):"fade-image"===t?gsap.to(e,{scrollTrigger:{trigger:e,start:"-20% 80%",end:"-20% 80%",toggleActions:"play none reverse none"},duration:1,opacity:1,y:0,transformOrigin:"0% 50% -50",ease:Back.easeOut,stagger:.02}):"fade-down"===t?gsap.to(e,{scrollTrigger:{trigger:e.closest("section"),start:"20% 60%",end:"20% 60%",toggleActions:"play none reverse none"},duration:1,opacity:1,y:0,transformOrigin:"0% 50% -50",ease:Back.easeOut,stagger:.02}):"fade-right"!==t&&"fade-left"!==t||gsap.to(e,{scrollTrigger:{trigger:e.closest("section"),start:"20% 60%",end:"20% 60%",toggleActions:"play none reverse none"},duration:1,opacity:1,x:0,transformOrigin:"0% 50% -50",ease:Back.easeOut,stagger:.02}))}),gsap.utils.toArray(".image.devio__animate").forEach(e=>{gsap.to(e,{scrollTrigger:{trigger:e,start:"top 80%",end:"bottom 20%",endTrigger:"footer",scrub:3,toggleClass:"anim",toggleActions:"play pause pause pause"},scale:1.5,duration:3,ease:"none"})})});
+jQuery(function () {
+  0 < $(".devio__animate").length &&
+    gsap.utils.toArray(".devio__animate").forEach((e) => {
+      var t = e.getAttribute("data-animate");
+      t &&
+        ("fade-up" === t
+          ? gsap.to(e, {
+              scrollTrigger: {
+                trigger: e.closest("section"),
+                start: "20% 60%",
+                end: "20% 60%",
+                toggleActions: "play none reverse none",
+              },
+              duration: 1,
+              opacity: 1,
+              y: 0,
+              transformOrigin: "0% 50% -50",
+              ease: Back.easeOut,
+              stagger: 0.02,
+            })
+          : "fade-image" === t
+          ? gsap.to(e, {
+              scrollTrigger: {
+                trigger: e,
+                start: "-20% 80%",
+                end: "-20% 80%",
+                toggleActions: "play none reverse none",
+              },
+              duration: 1,
+              opacity: 1,
+              y: 0,
+              transformOrigin: "0% 50% -50",
+              ease: Back.easeOut,
+              stagger: 0.02,
+            })
+          : "fade-down" === t
+          ? gsap.to(e, {
+              scrollTrigger: {
+                trigger: e.closest("section"),
+                start: "20% 60%",
+                end: "20% 60%",
+                toggleActions: "play none reverse none",
+              },
+              duration: 1,
+              opacity: 1,
+              y: 0,
+              transformOrigin: "0% 50% -50",
+              ease: Back.easeOut,
+              stagger: 0.02,
+            })
+          : ("fade-right" !== t && "fade-left" !== t) ||
+            gsap.to(e, {
+              scrollTrigger: {
+                trigger: e.closest("section"),
+                start: "20% 60%",
+                end: "20% 60%",
+                toggleActions: "play none reverse none",
+              },
+              duration: 1,
+              opacity: 1,
+              x: 0,
+              transformOrigin: "0% 50% -50",
+              ease: Back.easeOut,
+              stagger: 0.02,
+            }));
+    }),
+    gsap.utils.toArray(".image.devio__animate").forEach((e) => {
+      gsap.to(e, {
+        scrollTrigger: {
+          trigger: e,
+          start: "top 80%",
+          end: "bottom 20%",
+          endTrigger: "footer",
+          scrub: 3,
+          toggleClass: "anim",
+          toggleActions: "play pause pause pause",
+        },
+        scale: 1.5,
+        duration: 3,
+        ease: "none",
+      });
+    });
+});
