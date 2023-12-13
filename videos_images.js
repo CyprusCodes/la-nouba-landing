e5375c3986ea9d4918cb315405c910fb00454c7a
where.is.one <89258830+daniel-teddy@users.noreply.github.com>
2023-12-13 12:48:28 +0200

    functional auto event add, added video gallery, set up sheet to handle events

--- videos_images.js
+++ videos_images.js
@@ -1,40 +1,39 @@
-const SHEET_ID = "1-ER6EUPMzWpQhPh_-3p_5nQb4uUi8kMDepwpet77WmU";
-const SHEET_TITLE = "Events";
+const SHEET_ID = "1ChPRBmzmtJXhpP_s9Y1sWZ-E6mbdaXCdb6Ack-7i9No";
+const SHEET_TITLE = "videos_lanouba";
 const SHEET_RANGE = "A1:D101";
-const Column_IDs = ["event_id", "event_title", "event_image"];
+const Column_IDs = ["video_id", "v_link", "img_link"];
 
 const FULL_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tq=&gid=0&headers=1&tqx=out:csv&range=${SHEET_RANGE}`;
 
 fetch(FULL_URL)
   .then((res) => res.text())
   .then((csvData) => {
-    // console.log("CSV Data:", csvData);
     let rows = csvData.split("\n");
     let data = rows.map((row) => row.split('","'));
     const headers = data[0].map((header) => header.replace(/"/g, ""));
 
-    const slideContainer = document.getElementById("events_new_images");
+    const slideContainer = document.getElementById("video_imgs");
 
     for (let i = data.length - 1; i > 0; i--) {
       const rowData = data[i].map((value) => value.replace(/"/g, ""));
 
       const slideDiv = document.createElement("div");
-      slideDiv.className = "slide";
+      slideDiv.className = "image";
+      slideDiv.setAttribute("v_link", rowData[Column_IDs.indexOf("v_link")]);
 
       const obj = {};
       for (let j = 0; j < headers.length; j++) {
         obj[headers[j]] = rowData[j];
 
-        if (headers[j] === "event_image") {
+        if (headers[j] === "img_link") {
           const img = document.createElement("img");
           img.src = rowData[j];
           img.className = "lazy lazyload pic";
-          img.alt = obj.event_title;
+          img.loading = "lazy"; // Add this line for lazy loading
+          img.alt = obj.video_id; // Set a meaningful alt text
           slideDiv.appendChild(img);
         }
       }
-
-      slideDiv.innerHTML += `<p class="carousel_description">${obj.event_date}</p>`;
 
       slideContainer.appendChild(slideDiv);
     }
